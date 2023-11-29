b55f6c820ab55468aa53fbe38fff90374b6f1afa
Raphael Araújo <raphox.araujo@gmail.com>
2023-11-29 09:08:19 -0300

    Removed location

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
 
     if @post.save
-      render json: @post, status: :created, location: @post
+      render json: @post, status: :created
     else
       render json: @post.errors, status: :unprocessable_entity
     end
