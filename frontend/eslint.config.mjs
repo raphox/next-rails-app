580e2f76ae21daa8cd3731809e7447f41771f31b
Raphael Araújo <raphox.araujo@gmail.com>
2025-02-02 02:11:12 +0000

    Updated Yarn

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -11,7 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-,
   {
     rules: {
       "react-hooks/exhaustive-deps": "warn",
